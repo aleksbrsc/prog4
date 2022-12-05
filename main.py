d86d14c191f9fb891b90952feed25f6b455f3f4d
aleksbrsc <aleksbrsc@gmail.com>
2022-12-05 18:02:24 -0500

    a

--- main.py
+++ main.py
@@ -45,13 +45,13 @@ def check_identical_value(data, value):
 def view():
     with open ("resources.json", "r") as f:
         temp = json.load(f)
-        i = 0
+        i = 1
         for book in temp:
             name = book["name"]
             author = book["author"]
             yearPublished = book["yearPublished"]
             print(f"Book number: {i}")
-            print(f"Name: {name}\nAuthor: {author}\nYear published: {yearPublished}")
+            print(f"Name: {name}\nAuthor: {author}\nYear published: {yearPublished}\n")
             i += 1
 
 # searches for a book in the json file given its name and then prettyprints it
@@ -109,17 +109,14 @@ def delete():
             bookName = input("\u001b[90mEnter the book's name: \u001b[0m").strip()
         except: pass
         if check_identical_value(data, bookName) == True:
-            # figure out how to DELETE
-            print("\nWe have found the book following book:\n")
+            print("\nWe have found the book following book:")
             search(data, bookName) #pprints the book info
-            for value, book in enumerate(data):
+            for i, book in enumerate(data):
                 if book['name'] == bookName:
-                    print("Deletion successful")
-                    data.pop(value)
-                    #new_data.append(book)
+                    print("\u001b[32mThe book has been successfully deleted.\u001b[0m")
+                    data.pop(i)
                     with open ("resources.json", "w") as f:
                         json.dump(data, f, indent=4)
-                    
         else:
             print("\n\u001b[31mSuch a book does not exist in the Bing Chilling Library\n\u001b[90m(double-check spelling, capitalization or other errors)\u001b[0m")
             showMainMenu()
@@ -149,29 +146,33 @@ def showMainMenu():
                     # figure out search
                     print("\n\u001b[32mWe found the following book:\u001b[0m\n")
                     search(data, bookName)
-                    print("\nPlease select an option.\n(Create / List / Search / Update / Delete / Exit)\n")
                 else:
                     print("\n\u001b[31mSuch a book does not exist in the Bing Chilling Library\n\u001b[90m(double-check spelling, capitalization or other errors)\u001b[0m")
                     showMainMenu()
         
         # Update
         elif crud == "update" or crud == "u":
-            print("\nVery well.\n")
+            print("\nVery well. Enter the book you want to update.\n")
             with open ("resources.json", "r") as json_file:
                 data = json.load(json_file)
                 try:
                     bookName = input("\u001b[90mEnter the book's name: \u001b[0m").strip()
                 except: pass
-                # figure out DELETE and then throw create
                 if check_identical_value(data, bookName) == True:
-                    # DELETE
-                    # create()
-                    print("\nfigure out what to do here\n")
+                    print("\nWe have found the book following book:")
+                    search(data, bookName) #pprints the book info
+                    print("\n")
+                    for i, book in enumerate(data):
+                        if book['name'] == bookName:
+                            
+                            data.pop(i)
+                            #new_data.append(book)
+                            with open ("resources.json", "w") as f:
+                                json.dump(data, f, indent=4)
                 else:
                     print("\n\u001b[31mSuch a book does not exist in the Bing Chilling Library\n\u001b[90m(double-check spelling, capitalization or other errors)\u001b[0m")
                     showMainMenu()
 
-            print("Please select an option.\n(Create / List / Search / Update / Delete / Exit)\n")
         # Delete
         elif crud == "delete" or crud == "del" or crud == "d":
             print("\nVery well.\n")
@@ -180,10 +181,9 @@ def showMainMenu():
         elif crud == "exit" or crud == "e":
             print("\nVery well.\n")
             quit()
-        else: print("\nThat was not a valid option. Please try again\n(Create / List / Search / Update / Delete / Exit)\n")
 
 # starts program and asks user if they want to continue
-def run(): 
+def run():
     print("\n\u001b[90m~\u001b[0m MAIN MENU\n\nYou have arrived at Bing Chilling Library. Would you like to enter?\n(yes / no)\n")
     while True:
         cont = input("\u001b[90m> \u001b[0m").lower()
