40d4ce450353d07063be08538497603770291715
aleksbrsc <aleksbrsc@gmail.com>
2022-12-05 19:28:36 -0500

    done

--- main.py
+++ main.py
@@ -64,22 +64,17 @@ def search(data, name):
 def create():
 # input validation for name, author, year published
     while True:
-        try:
-            bookName = input("\u001b[90mEnter the book's name: \u001b[0m").strip()
-        except: pass
+        bookName = input("\u001b[90mEnter the book's name: \u001b[0m").strip()
         with open ("resources.json", "r") as json_file:
             data = json.load(json_file)
-            if check_identical_value(data, bookName) == True:
+            if check_identical_value(data, bookName):
                 print("\n\u001b[31mThis book name already exists in the Bing Chilling Library.\u001b[0m")
                 showMainMenu()
         if contains_letters(bookName):
             break
         else: print("\nInvalid book name.\n")
-            
     while True:
-        try:
-            bookAuthor = input("\u001b[90mEnter the author's name: \u001b[0m").lower().strip()
-        except: pass
+        bookAuthor = input("\u001b[90mEnter the author's name: \u001b[0m").lower().strip()
         if contains_letters(bookAuthor) and (contains_numbers(bookAuthor) == False):
             break
         else: print("\nInvalid author name.\n")
@@ -102,77 +97,68 @@ def create():
 def update():
     with open ("resources.json", "r") as json_file:
         data = json.load(json_file)
-        try:
-            bookName = input("\u001b[90mEnter the book's name: \u001b[0m").strip()
-        except: pass
-        if check_identical_value(data, bookName) == True:
-            print("\nWe have found the book following book:")
-            search(data, bookName) #prints the book info
-            while True:
-                try:
-                    updateKind = input("\nEnter a valid kind of update.\n(name / author / year published)\n\n\u001b[90m> \u001b[0m").lower()
-                except: pass
-                #update name
-                if updateKind == "name" or updateKind == "n":
-                    while True:
-                        try:
-                            bookNewName = input("\n\u001b[90mEnter the new book name: \u001b[0m").strip()
-                        except: pass
-                        with open ("resources.json", "r") as json_file:
-                            data = json.load(json_file)
-                            if check_identical_value(data, bookNewName) == True:
-                                print("\n\u001b[31mThis book name already exists in the Bing Chilling Library.\u001b[0m")
-                                showMainMenu()
-                        if contains_letters(bookNewName):
-                            break
-                        else: print("\nInvalid book name.\n")
-                    for i, book in enumerate(data):
-                        if book['name'] == bookName:
-                            data[i]['name'] = bookNewName
-                            with open ("resources.json", "w") as f:
-                                json.dump(data, f, indent=4)
-                            print("\n\u001b[32mYour book has been successfully edited.\u001b[0m")
-                            showMainMenu()
-                #update author
-                elif updateKind == "author" or updateKind == "a":
-                    while True:
-                        try:
-                            bookAuthor = input("\n\u001b[90mEnter the new author name: \u001b[0m").lower().strip()
-                        except: pass
-                        if contains_letters(bookAuthor) and (contains_numbers(bookAuthor) == False):
-                            break
-                        else: print("\nInvalid author name.\n")
-                    for i, book in enumerate(data):
-                        if book['name'] == bookName:
-                            data[i]['author'] = bookAuthor
-                            with open ("resources.json", "w") as f:
-                                json.dump(data, f, indent=4)
-                            print("\n\u001b[32mYour book has been successfully edited.\u001b[0m")
-                            showMainMenu()
-                #update year published
-                elif updateKind == "year published" or updateKind == "year" or updateKind == "y":
-                    updateSelected = 3
-                    while True:
-                        bookYear = 2023
-                        try:
-                            bookYear = int(input("\n\u001b[90mEnter the new year of publication: \u001b[0m"))
-                        except: pass
-                        if bookYear < 2023:
-                            break
-                        else: print("\nInvalid year of publication\n")
-                    for i, book in enumerate(data):
-                        if book['name'] == bookName:
-                            data[i]["yearPublished"] = bookYear
-                            with open ("resources.json", "w") as f:
-                                json.dump(data, f, indent=4)
-                            print("\n\u001b[32mYour book has been successfully edited.\u001b[0m")
-                            showMainMenu()
-                else: 
-                    print("\n\u001b[31mThat was not a valid option. Please try again\u001b[0m")
-            
-        else:
-            print("\n\u001b[31mSuch a book does not exist in the Bing Chilling Library\n\u001b[90m(double-check spelling, capitalization or other errors)\u001b[0m")
-            showMainMenu()
+    bookName = input("\u001b[90mEnter the book's name: \u001b[0m").strip()
+    if check_identical_value(data, bookName) == True:
+        print("\nWe have found the book following book:")
+        search(data, bookName) #prints the book info
+        while True:
+            try:
+                updateKind = input("\nEnter a valid kind of update.\n(name / author / year published)\n\n\u001b[90m> \u001b[0m").lower()
+            except: pass
+            #update name
+            if updateKind == "name" or updateKind == "n":
+                while True:
+                    bookNewName = input("\n\u001b[90mEnter the new book name: \u001b[0m").strip()
+                    if check_identical_value(data, bookNewName) == True:
+                        print("\n\u001b[31mThis book name already exists in the Bing Chilling Library.\u001b[0m")
+                        showMainMenu()
+                    if contains_letters(bookNewName):
+                        break
+                    else: print("\nInvalid book name.\n")
+                for i, book in enumerate(data):
+                    if book['name'] == bookName:
+                        data[i]['name'] = bookNewName
+                        with open ("resources.json", "w") as f:
+                            json.dump(data, f, indent=4)
+                        print("\n\u001b[32mYour book has been successfully edited.\u001b[0m")
+                        showMainMenu()
+            #update author
+            elif updateKind == "author" or updateKind == "a":
+                while True:
+                    bookAuthor = input("\n\u001b[90mEnter the new author name: \u001b[0m").lower().strip()
+                    if contains_letters(bookAuthor) and (contains_numbers(bookAuthor) == False):
+                        break
+                    else: print("\nInvalid author name.\n")
+                for i, book in enumerate(data):
+                    if book['name'] == bookName:
+                        data[i]['author'] = bookAuthor
+                        with open ("resources.json", "w") as f:
+                            json.dump(data, f, indent=4)
+                        print("\n\u001b[32mYour book has been successfully edited.\u001b[0m")
+                        showMainMenu()
+            #update year published
+            elif updateKind == "year published" or updateKind == "year" or updateKind == "y":
+                updateSelected = 3
+                while True:
+                    bookYear = 2023
+                    try:
+                        bookYear = int(input("\n\u001b[90mEnter the new year of publication: \u001b[0m"))
+                    except: pass
+                    if bookYear < 2023:
+                        break
+                    else: print("\nInvalid year of publication\n")
+                for i, book in enumerate(data):
+                    if book['name'] == bookName:
+                        data[i]["yearPublished"] = bookYear
+                        with open ("resources.json", "w") as f:
+                            json.dump(data, f, indent=4)
+                        print("\n\u001b[32mYour book has been successfully edited.\u001b[0m")
+                        showMainMenu()
+            else: 
+                print("\n\u001b[31mThat was not a valid option. Please try again\u001b[0m")
+    else:
+        print("\n\u001b[31mSuch a book does not exist in the Bing Chilling Library\n\u001b[90m(double-check spelling, capitalization or other errors)\u001b[0m")
+        showMainMenu()
 
 # prompts user to enter a book to be deleted from the json file
 def delete():
@@ -199,7 +185,7 @@ def delete():
 def showMainMenu():
     while True:
         crud = input("\nPlease select an option.\n(Create / List / Search / Update / Delete / Exit)\n\n\u001b[90m> \u001b[0m").lower()
-        # CREATE
+        # CreateE
         if crud == "create" or crud == "c":
             print("\nVery well.\n")
             create()
@@ -207,7 +193,6 @@ def showMainMenu():
         elif crud == "List" or crud == "l":
             print("\nHere is a list of all books in the Bing Chilling Library:\n")
             view()
-
         # Search
         elif crud == "search" or crud == "s" or crud == "serach":
             print("\nVery well.\n")
@@ -223,12 +208,10 @@ def showMainMenu():
                 else:
                     print("\n\u001b[31mSuch a book does not exist in the Bing Chilling Library\n\u001b[90m(double-check spelling, capitalization or other errors)\u001b[0m")
                     showMainMenu()
-        
         # Update
         elif crud == "update" or crud == "u":
             print("\nVery well. Enter the book you want to update.\n")
             update()
-
         # Delete
         elif crud == "delete" or crud == "del" or crud == "d":
             print("\nVery well.\n")
